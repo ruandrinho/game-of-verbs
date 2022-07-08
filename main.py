5edb95a31d3d4af3996b83b16d3acd4b58f543dd
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-08 19:16:23 +0500

    Setup DialogFlow

--- main.py
+++ main.py
@@ -4,16 +4,36 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler
 from telegram.ext import MessageHandler, Filters
+from google.cloud import dialogflow
 
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Здравствуйте")
+                             text="Привет, напиши что-нибудь")
+
+
+def chat(update: Update, context: CallbackContext):    
+    project_id = os.getenv('GOOGLECLOUD_PROJECT_ID')
+    session_id = update.effective_chat.id
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    reply = get_dialogflow_reply(session, session_client, update.message.text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply)
 
 
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=update.message.text)
+
+
+def get_dialogflow_reply(session, session_client, text, language_code='ru-RU'):
+    text_input = dialogflow.TextInput(text=text,
+                                      language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={'session': session, 'query_input': query_input}
+    )
+    return response.query_result.fulfillment_text
 
 
 def main():
@@ -27,10 +47,15 @@ def main():
 
     updater = Updater(os.getenv('TELEGRAM_BOT_TOKEN'), use_context=True)
     dispatcher = updater.dispatcher
+
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+    chat_handler = MessageHandler(Filters.text & (~Filters.command), chat)
+    dispatcher.add_handler(chat_handler)
+
     updater.start_polling()
     updater.idle()
 
